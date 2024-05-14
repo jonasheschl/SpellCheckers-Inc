906436f521fb9a8bf1f37df696447c795d638333
Jonas Heschl <heschl@protonmail.com>
2024-05-14 17:36:47 +0200

    Add text to index

--- magicians-agency/src/index.php
+++ magicians-agency/src/index.php
@@ -2,4 +2,8 @@
 include_once "header.php";
 ?>
 
-<span>lorem ipsum</span>
+<div class="container my-8">
+    <h1>Bringing magic to you since 1970-1-1</h1>
+
+    <p>Refer to the various sub-pages for information and configuration.</p>
+</div>
