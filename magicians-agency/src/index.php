76e0da965daa503809bb749d8ec61782c08012a4
Jonas Heschl <heschl@protonmail.com>
2024-05-15 12:00:00 +0200

    Streamline custom text on index

--- magicians-agency/src/index.php
+++ magicians-agency/src/index.php
@@ -2,8 +2,6 @@
 include_once "header.php";
 ?>
 
-<div class="container my-8">
-    <h1>Bringing magic to you since 1970-1-1</h1>
+<h1>Bringing magic to you since 1970</h1>
 
-    <p>Refer to the various sub-pages for information and configuration.</p>
-</div>
+<p>Refer to the various sub-pages for information and configuration.</p>
